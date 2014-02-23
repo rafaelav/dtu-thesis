af844e822d17a53332dffe9ce4d001f703705f9c
rafaelav <rafaela.voiculescu@gmail.com>
2014-02-23 12:18:02 +0100

    removed unused stuff

--- graphics/bssids_signals_time_plot.py
+++ graphics/bssids_signals_time_plot.py
@@ -18,45 +18,6 @@ TIME_BEFORE_INTERRUPT = 60*30 # 30 mins
 
 week   = {0:'Mon', 1:'Tue', 2:'Wed', 3:'Thu',  4:'Fri', 5:'Sat', 6:'Sun'}
 
-def get_info_on_time(given_list):
-    text_list = []
-    
-    for tval in given_list:
-        date_val = datetime.datetime.utcfromtimestamp(int(tval))
-        text_list.append(week[date_val.weekday()]+"\n"+str(date_val.hour)+":"+str(date_val.minute))
-    
-    return text_list 
-
-def get_real_times_for_ticklocs(data_to_plot, count):
-    """ Returns count timestamps to be ploted on the x axis"""
-    timestamps = []
-    for key in data_to_plot.iterkeys():
-        for tuple_of_lists in data_to_plot[key]: 
-            time_list = tuple_of_lists[0]
-
-            for x in time_list:
-                if x not in timestamps:
-                    timestamps.append(x)
-    
-    timestamps = sorted(timestamps, key=lambda x: x)
-    
-    if (len(timestamps)%count)%2 == 0:
-        step = len(timestamps)/count
-    else:
-        step = len(timestamps)/count + 1
-    
-    #print("First/last timestamps: "+str(timestamps[0])+" "+str(timestamps[len(timestamps)-1]))
-    timestamps_to_return = []
-    
-    crt = 0
-    added = 0
-    while added < count:
-        timestamps_to_return.append(timestamps[crt])
-        crt = crt + step
-        added = added + 1
-    
-    #print(timestamps_to_return)
-    return timestamps_to_return
 def get_utc_from_epoch(epoch_time):
     date_val = datetime.datetime.utcfromtimestamp(int(epoch_time))
     return week[date_val.weekday()]+"\n"+str(date_val.hour)+":"+str(date_val.minute)
@@ -115,14 +76,7 @@ def plot_for_bssid(color_to_use, data_to_plot, username, start_day, days_to_cons
                 ax.plot(x_time_list, y_rssi_list, '-', color=color_to_use[key])
                 first = False
 
-    # change labels
-    """locs, labels = plt.xticks()
-    ticks_list = ax.xaxis.get_majorticklocs()
-    count_ticks = len(ticks_list)
-    real_moments_ticks = get_real_times_for_ticklocs(data_to_plot, count_ticks)
-    labels = get_info_on_time(real_moments_ticks) # new labels
-    plt.xticks(locs, labels)"""
-    
+    # change labels    
     no_of_ticks = (end_time - start_time)/(time_bins_len*60) + 1
     print(no_of_ticks)
     ticks, labels_utc = get_xticks_xlabels(start_time,end_time, no_of_ticks, time_bins_len)
@@ -180,39 +134,3 @@ def prepared_data_to_plot_for_each_bssid(user_file, start_day, days_to_consider,
     print("Data for user "+user_file+" prepared for plotting. Moving on to actually plotting...")            
     plotted_fig = plot_for_bssid(colors, data_to_plot, username, start_day, days_to_consider, most_common_bssids_legend, time_bins_len, start_time, end_time)#, time_list)
     return plotted_fig
-    
-"""
-def prepare_data_and_start_plot_bssid_over_time(user_file, start_day, days_to_consider, n_best_signal_bssids, m_most_popular_bssids, max_in_legend):
-    # get data from file
-    user_data = user_data_handler.retrieve_data_from_user(user_file,start_day,days_to_consider)
-    
-    # get unique timestamps from data
-    timestamps = user_data_handler.get_unique_timestamps(user_data)
-    
-    # find out which are the bssids which appear most common ( first m_most_popular_bssids)
-    most_common_bssids  = user_data_handler.get_most_common_bssids(user_data, m_most_popular_bssids)
-    
-    # find out most popular max_in_legend bssids (the ones who will be put in the plot legend)
-    if m_most_popular_bssids > max_in_legend or m_most_popular_bssids == -1:
-        most_common_bssids_legend  = user_data_handler.get_most_common_bssids(user_data, max_in_legend)
-    else:
-        most_common_bssids_legend = most_common_bssids
-    # git fingerprints which contain only most popular bssids and get only first n_best_signal_bssids for each
-    fingerprints = user_data_handler.get_fingerprints(user_data, timestamps, n_best_signal_bssids, most_common_bssids)
-    
-    # find out bssid which appear in timestamps
-    bssids = user_data_handler.get_unique_bssid_from_fingerprints(fingerprints)
-    
-    # find out at which times do each bssid appears
-    bssid_occurences = user_data_handler.get_bssids_info_in_time(fingerprints, bssids)
-    
-    # get colors for each bssid
-    color_codes = user_data_handler.generate_color_codes_for_bssid(bssids)
-    
-    #time_list = data.get_ordered_time_list(fingerprints)
-    # plot
-    print("Data for user "+user_file+" retrieved. Moving on to preparing the data for plotting...")
-    prepared_data_to_plot_for_each_bssid(user_file, start_day, days_to_consider, bssid_occurences, color_codes, most_common_bssids_legend)#, time_list)
-"""
-#for i in range(1,2):
-    #prepare_data_and_start_plot_bssid_over_time("user_"+str(i)+"_sorted",0,1,-1,-1,10)
