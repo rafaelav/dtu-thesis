3ca06b692b6d71d7ac899369d8c2ba90bbdd30f2
rafaelav <rafaela.voiculescu@gmail.com>
2014-02-27 16:45:25 +0100

    Added interrupt time between moments for running average plots

--- graphics/bssids_rssi_running_avg_time_plot.py
+++ graphics/bssids_rssi_running_avg_time_plot.py
@@ -8,6 +8,7 @@ sys.path.append( ".." )
 import datetime
 import matplotlib.pyplot as plt
 NO_SECS_PER_MIN = 60
+MAX_INTERRUPT = 2 # 2 mins
 week   = {0:'Mon', 1:'Tue', 2:'Wed', 3:'Thu',  4:'Fri', 5:'Sat', 6:'Sun'}
 
 def get_utc_from_epoch(epoch_time):
@@ -33,6 +34,42 @@ def get_xticks_xlabels_from_time(data_start_time, data_end_time, no_of_ticks, be
     dates_utc.append(get_utc_from_epoch(timestamp))
         
     return dates_epoch, dates_utc
+
+def get_only_non_null_data(dates_epoch, averages):
+    """Return only the non null averages and associated dates"""
+    dates_result = []
+    avgs_result = []
+    for i in range(0,len(averages)):
+        if averages[i] != 0:
+            dates_result.append(dates_epoch[i])
+            avgs_result.append(averages[i])
+    return dates_result,avgs_result
+
+def get_segments_with_no_pause(dates_epoch, averages):
+    if len(dates_epoch)!=len(averages):
+        print("ERROR! Differnce between length of dates list and averages list")
+        
+    dates_list = []
+    averages_list = []
+    crt_avg_strike = []
+    crt_dates_strike = []
+
+    crt_avg_strike.append(averages[0])
+    crt_dates_strike.append(dates_epoch[0])
+        
+    for i in range(1,len(dates_epoch)):
+        if  dates_epoch[i]-dates_epoch[i-1] < MAX_INTERRUPT * NO_SECS_PER_MIN:
+            crt_avg_strike.append(averages[i])
+            crt_dates_strike.append(dates_epoch[i])
+        else:
+            dates_list.append(crt_dates_strike)
+            averages_list.append(crt_avg_strike)
+            crt_avg_strike = []
+            crt_dates_strike = []
+            crt_avg_strike.append(averages[i])
+            crt_dates_strike.append(dates_epoch[i])
+    return dates_list, averages_list
+            
 
 def plot_bssid_rssi_avg_over_time(full_data, running_avg_dict, colors_dict, username, days_to_consider, time_bins_len,option,time_window, start_time, end_time):
     fig_list = []
@@ -64,9 +101,21 @@ def plot_bssid_rssi_avg_over_time(full_data, running_avg_dict, colors_dict, user
         print(time_bins_len,no_of_ticks)
         ticks, labels_utc = get_xticks_xlabels_from_time(start_time, end_time, no_of_ticks, time_bins_len)#(dates_epoch, no_of_ticks)
         
-        #plt.bar(dates_epoch, averages, width, color=colors_dict[bssid])
-        plt.plot(dates_epoch, averages, 'D-', color=colors_dict[bssid])
+        dates_epoch_non_null, averages_non_null = get_only_non_null_data(dates_epoch, averages)
+        dates_list, averages_list = get_segments_with_no_pause(dates_epoch_non_null, averages_non_null) 
+        
+        if len(dates_list) != len(averages_list):
+            print("ERROR! Not same len for dates and averages lists")
+            return None
+        
+        for i in range(0,len(dates_list)):
+            plt.plot(dates_list[i], averages_list[i], 'D-', color=colors_dict[bssid])
+        
         plt.xticks(ticks, labels_utc, rotation = 90)
+                    
+        #plt.bar(dates_epoch, averages, width, color=colors_dict[bssid])
+        """plt.plot(dates_epoch, averages, 'D-', color=colors_dict[bssid])
+        plt.xticks(ticks, labels_utc, rotation = 90)"""
         
         plt.title("Running average signal per time window ("+str(time_window)+" mins) for bssid "+str(bssid)+" Plot over (days): "+str(days_to_consider)+" User: "+username)
         plt.xlabel("Time bins", fontsize=10)
